eb78e7dd50bcc2df7b674f60358c410a4bdb01fa
Sergey Semichev <ssemichev@gmail.com>
2017-04-21 19:41:51 -0400

    Travis CI build configuration

--- scripts/deploy.rb
+++ scripts/deploy.rb
@@ -1,19 +1,8 @@
 #!/usr/bin/env ruby
 
-def exclusion
-  %w[ scripts ]
-end
+require 'utils.rb'
 
-def changes
-  all = %x[git diff --name-only $TRAVIS_COMMIT_RANGE].split("\n")
-  all.select { |path| path[/[\w-]*\/.*/] }
-end
-
-all_folders = changes.map { |f| f[/[\w-]*/] }.uniq
-
-dockers_to_deploy = all_folders - exclusion
-
-dockers_to_deploy.each do |docker|
+dockers_to_build.each do |docker|
   %x[cd $docker && make push]
 end.empty? and begin
   puts 'Nothing to deploy'
