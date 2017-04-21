a3dbbddb4c941cdfb7083ca09b71409f2277f4da
Sergey Semichev <ssemichev@gmail.com>
2017-04-21 19:03:12 -0400

    Travis CI build configuration

--- scripts/script-phase.rb
+++ scripts/script-phase.rb
@@ -1,5 +1,33 @@
 #!/usr/bin/env ruby
 
-dockers = %x[git diff --name-only $TRAVIS_COMMIT_RANGE].split("\n").select{ |f| f[/[\w-]*\/.*/] }.map { |f| f[/[\w-]*/] }.uniq
+def exclusion
+  %w[ scripts ]
+end
 
-puts dockers
+def print_env
+  puts "TRAVIS_BRANCH: #{ENV['TRAVIS_BRANCH']"
+  puts "TRAVIS_TAG: #{ENV['TRAVIS_TAG']"
+  puts "TRAVIS_PULL_REQUEST: #{ENV['TRAVIS_PULL_REQUEST']"
+  puts "TRAVIS_COMMIT_RANGE #{ENV['TRAVIS_COMMIT_RANGE']"
+end
+
+def changes
+  all = %x[git diff --name-only $TRAVIS_COMMIT_RANGE].split("\n")
+  all.select { |path| path[/[\w-]*\/.*/] }
+end
+
+print_env
+puts "exclusion: #{exclusion}"
+puts "changes: #{changes}"
+
+all_folders = changes.map { |f| f[/[\w-]*/] }.uniq
+puts "all_folders: #{all_folders}"
+
+dockers_to_build = all_folders - exclusion
+puts "dockers_to_build: #{dockers_to_build}"
+
+dockers_to_build.each do |docker|
+  puts docker
+end.empty? and begin
+  puts 'Nothing to build'
+end
