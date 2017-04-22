abdfe5af12c2eec79e65355de07327bcfe729ad7
Sergey Semichev <ssemichev@gmail.com>
2017-04-21 21:09:57 -0400

    Travis CI build configuration

--- scripts/build.rb
+++ scripts/build.rb
@@ -10,7 +10,8 @@ puts "dockers_to_build: #{dockers_to_build}"
 
 dockers_to_build.each do |docker|
   puts "Building #{docker} image..."
-  %x[cd #{docker} && make pre-release]
+  output = %x[cd #{docker} && make pre-release]
+  puts output
   puts "#{docker}... image has been built"
 end.empty? and begin
   puts 'Nothing to build'
--- scripts/deploy.rb
+++ scripts/deploy.rb
@@ -4,7 +4,7 @@ require_relative 'utils.rb'
 
 dockers_to_build.each do |docker|
   puts "Deploying #{docker} image..."
-  outputs = %x[cd #{docker} && make push]
+  output = %x[cd #{docker} && make push]
   puts output
   puts "#{docker}... image has been deployed"
 end.empty? and begin
